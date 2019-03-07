86032dbe673931279dc865ae4c22591c2e4e2e03
ekrkrdl <ekrkrdl@naver.com>
2019-03-07 10:38:49 +0900

    C와 유사한 java 문법 맛보기
    흠.. 여기에 뭐 적으셨는지 못봤다..

---
+++ 0307/src/review_C/EscapeChar.java
@@ -0,0 +1,10 @@
+package review_C;
+
+public class EscapeChar {
+	public static void main(String[] args) {
+		System.out.println("AB"+'\b'+'C');
+		System.out.println("AB"+'\t'+'C');
+		System.out.println("AB"+'\n'+'C');
+		System.out.println("AB"+'\r'+'C');
+	}
+}
---
+++ 0307/src/review_C/IfElse.java
@@ -0,0 +1,13 @@
+package review_C;
+
+public class IfElse {
+	public static void main() {
+		int x = 5;
+		
+		if(x>=5) {
+			System.out.println("x는 5이상입니다.");
+		} else {
+			System.out.println("x는 5미만입니다.");
+		}
+	}
+}
